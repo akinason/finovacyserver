c2eae2ae53212bee1026cb36a2eb8851c46cb95c
Awa Kinason <kinason42@gmail.com>
2019-08-02 18:41:49 +0100

    added corsheader module

--- finovacyserver/settings/base.py
+++ finovacyserver/settings/base.py
@@ -36,12 +36,15 @@ INSTALLED_APPS = [
 
     'rest_framework.authtoken',
 
+
     'api.apps.ApiConfig',
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
